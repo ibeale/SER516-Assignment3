3bfde8d6cc2f841f03a504bf7ad009b0d13575a2
Isaac Beale <isaacdbeale@gmail.com>
2021-02-12 12:49:36 -0700

    Check how a cell's state should change added reset to blank state added tests US 4,9,10 Tasks 16, 19, 20 ,21

--- GameOfLife/src/Model/State.java
+++ GameOfLife/src/Model/State.java
@@ -9,6 +9,14 @@ public class State {
     private int _width = 15;
     private int _height = 15;
     private boolean[][] _currentState;
+
+    public enum Conditions{
+        Overpopulated,
+        Solitary,
+        Survives,
+        Populate,
+        NoChange
+    }
     
     private State(){
         this._currentState = new boolean[this._width][this._height];
@@ -24,9 +32,55 @@ public class State {
         return instance;
     }
 
+    public void resetState(){
+        instance = new State();
+    }
+
     //Sets the status of a single cell, and returns the new status.
     public boolean toggleSingleCell(int x, int y){
         this._currentState[x][y] = !this._currentState[x][y];
         return(this._currentState[x][y]);
     }
+
+    public boolean getCellState(int x, int y){
+        return(this._currentState[x][y]);
+    }
+
+    private int getNumAdjacent(int x, int y){
+        int count = 0;
+        int[] directions = {-1, 0, 1};
+        for(int xDir : directions){
+            for(int yDir : directions){
+                int xCoord = x + xDir;
+                int yCoord = y + yDir;
+                if(!(xDir == 0 && yDir ==0) && (xCoord >= 0 && xCoord <= this._width) && (yCoord >= 0 && yCoord <= this._height) && getCellState(xCoord, yCoord)){
+                    count += 1;
+                }
+            }
+        }
+        return count;
+
+    }
+
+    public Conditions getConditionofCell(int x, int y){
+        int numAdjacent = getNumAdjacent(x, y);
+        boolean cellState = getCellState(x, y);
+        if(cellState && numAdjacent <= 1){
+            return Conditions.Solitary;
+        }
+        else if(cellState && numAdjacent >= 4){
+            return Conditions.Overpopulated;
+        }
+        else if(cellState && (numAdjacent == 2 || numAdjacent == 3)){
+            return Conditions.Survives;
+        }
+        else if(!cellState && numAdjacent == 3){
+            return Conditions.Populate;
+        }
+        else{
+            return Conditions.NoChange;
+        }
+
+    }
 }
+
